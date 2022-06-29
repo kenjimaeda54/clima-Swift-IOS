28ccfb0d0715dc4b7f9220e174ed63e3b2a03212
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-06-29 09:00:42 -0300

    feat: learn methods text input

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+//UITextFieldDelegate e um protocolo para administrar o valor digitado no campo do input
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextInput: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextInput.delegate = self
+    }
+    
+    @IBAction func buttonSearch(_ sender: UIButton) {
+        //forco fechar o teclado
+        searchTextInput.endEditing(true)
+        print(searchTextInput.text!)
+    }
+    
+    //metodo disponivel no UITextFieldDelegate, ele e chamado quando usuario clica no teclado para
+    //finalizar a edicao,a propriedade e return key no story board
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //forco fechar o teclado
+        searchTextInput.endEditing(true)
+        print(searchTextInput.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text == "" {
+            searchTextInput.placeholder = "Type something  here"
+            return false
+        }else {
+            searchTextInput.placeholder = "Search"
+            return true
+        }
+    }
+    
+    //metodo disponivel no UITextFieldDelegate
+    //esse metodo e chamado quando finaliza a edicao
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        searchTextInput.text = ""
+    }
+    
+}
+
